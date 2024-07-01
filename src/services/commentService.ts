b1208a1c3fd17cee18c68ee27d4bc5f091ba87de
陳勝利  （ひねる まさと） <thangloitran406@gmail.com>
2024-07-01 18:12:25 +0700

    update: validate replyTo

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -12,11 +12,13 @@ export class CommentService extends AbstractService<Comment> {
     };
 
     create: (data: Comment) => Promise<Comment> = async (data: Comment) => {
-        return Comment.create({ ...data.dataValues });
+        const replyTo = data.replyTo;
+        return Comment.create({ ...data.dataValues, replyTo: replyTo == 0 ? null : replyTo});
     };
 
     update: (id: number, data: Comment) => Promise<[affectedCount: number]> = async (id: number, data: Comment) => {
-        return Comment.update({ ...data.dataValues }, { where: { id } });
+        const replyTo = data.replyTo;
+        return Comment.update({ ...data.dataValues, replyTo: replyTo == 0 ? null : replyTo}, { where: { id } });
     };
 
     delete: (id: number) => Promise<number> = async (id: number) => {
