71ebc44d02a0fb5851f3ceb9c9d9b201a082b73e
陳勝利  （ひねる まさと） <thangloitran406@gmail.com>
2024-07-01 15:11:37 +0700

    update: authorization comments

--- src/routes/commentApi.ts
+++ src/routes/commentApi.ts
@@ -2,11 +2,12 @@ import express from "express";
 const commentApiRouter = express.Router();
 import commentController from "@/controllers/commentController";
 import { noUpload } from "@/configs/multer";
+import { authorization } from "@/middlewares";
 
 commentApiRouter.get("/", commentController.findAll);
 commentApiRouter.get("/:id", commentController.findById);
-commentApiRouter.post("/", noUpload, commentController.create);
-commentApiRouter.put("/:id", noUpload, commentController.update);
-commentApiRouter.delete("/:id", commentController.delete);
+commentApiRouter.post("/", noUpload, authorization, commentController.create);
+commentApiRouter.put("/:id", noUpload, authorization, commentController.update);
+commentApiRouter.delete("/:id", authorization, commentController.delete);
 
 export { commentApiRouter };
